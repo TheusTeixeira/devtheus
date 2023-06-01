64d3d96f768f5cfd76902c771131ad240e3c4538
Matheus Teixeira <109447512+TheusTeixeira@users.noreply.github.com>
2023-05-31 23:35:43 -0300

    all

--- js/scriptpr.js
+++ js/scriptpr.js
@@ -1,21 +1,17 @@
-// Array para armazenar os produtos adicionados ao carrinho
-let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-
-// Função para adicionar um produto ao carrinho
-function adicionarAoCarrinho(nome, preco) {
-  let produtoExistente = carrinho.find(function(item) {
-    return item.nome === nome;
-  });
-
-  if (produtoExistente) {
-    // Atualiza a quantidade e o valor do produto existente
-    produtoExistente.quantidade++;
-    produtoExistente.valor = produtoExistente.quantidade * preco;
-  } else {
-    // Adiciona o produto ao carrinho
-    carrinho.push({ nome, preco, quantidade: 1, valor: preco });
+function atualizarRelogio() {
+    var dataAtual = new Date();
+    var horas = formatarNumero(dataAtual.getHours());
+    var minutos = formatarNumero(dataAtual.getMinutes());
+    var segundos = formatarNumero(dataAtual.getSeconds());
+  
+    document.getElementById("horas").innerText = horas;
+    document.getElementById("minutos").innerText = minutos;
+    document.getElementById("segundos").innerText = segundos;
   }
-
-  localStorage.setItem('carrinho', JSON.stringify(carrinho)); // Armazena o carrinho no localStorage
-  alert('Produto adicionado ao carrinho: ' + nome); // Exibe uma mensagem de alerta
-}
+  
+  function formatarNumero(numero) {
+    return numero < 10 ? "0" + numero : numero;
+  }
+  
+  setInterval(atualizarRelogio, 1000);
+  
